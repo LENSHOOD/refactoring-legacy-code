60e9738604dc7680658f093456a026b1480691af
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-03-12 23:06:58 +0800

    refactor: replace distributed lock to double lock

--- src/main/java/cn/xpbootcamp/legacy_code/WalletTransaction.java
+++ src/main/java/cn/xpbootcamp/legacy_code/WalletTransaction.java
@@ -2,24 +2,23 @@ package cn.xpbootcamp.legacy_code;
 
 import cn.xpbootcamp.legacy_code.enums.STATUS;
 import cn.xpbootcamp.legacy_code.service.WalletService;
-import cn.xpbootcamp.legacy_code.utils.lock.DistributedLock;
 import cn.xpbootcamp.legacy_code.utils.IdGenerator;
+import cn.xpbootcamp.legacy_code.utils.lock.DoubleLockManager;
 import cn.xpbootcamp.legacy_code.vo.TransactionInfo;
 import javax.transaction.InvalidTransactionException;
 
 public class WalletTransaction {
     private static final int EXPIRED_INTERVAL = 1728000000;
+
     private TransactionInfo transactionInfo;
-
     private String id;
-
     private STATUS status = STATUS.TO_BE_EXECUTED;
     private Long createdTimestamp;
 
-    private DistributedLock distributedLock;
-
+    private DoubleLockManager.DoubleLock lock;
     private WalletService walletService;
-    public WalletTransaction(TransactionInfo transactionInfo, DistributedLock distributedLock, WalletService walletService)
+
+    public WalletTransaction(TransactionInfo transactionInfo, DoubleLockManager doubleLockManager, WalletService walletService)
             throws InvalidTransactionException {
         if (transactionInfo == null) {
             throw new InvalidTransactionException("This is an invalid transaction");
@@ -27,8 +26,8 @@ public class WalletTransaction {
         this.transactionInfo = transactionInfo;
         this.id = IdGenerator.generateTransactionId();
         this.createdTimestamp = System.currentTimeMillis();
-        this.distributedLock = distributedLock;
         this.walletService = walletService;
+        this.lock = doubleLockManager.obtainLock(transactionInfo.getBuyerId(), transactionInfo.getSellerId());
     }
 
     public boolean execute() {
@@ -41,7 +40,7 @@ public class WalletTransaction {
     }
 
     boolean doMoveMoney() {
-        if (!tryLock()) {
+        if (!lock.tryLock()) {
             return false;
         }
         try {
@@ -55,36 +54,12 @@ public class WalletTransaction {
             this.status = STATUS.EXECUTED;
             return true;
         } finally {
-            unlock();
+            lock.unlock();
         }
     }
 
     boolean isExpired() {
         return System.currentTimeMillis() - createdTimestamp > EXPIRED_INTERVAL;
-    }
-
-    boolean tryLock() {
-        if (!distributedLock.lock(String.valueOf(transactionInfo.getBuyerId()))) {
-            return false;
-        }
-
-        boolean acquired = false;
-        try {
-            acquired = distributedLock.lock(String.valueOf(transactionInfo.getSellerId()));
-            return acquired;
-        } finally {
-            if (!acquired) {
-                distributedLock.unlock(String.valueOf(transactionInfo.getBuyerId()));
-            }
-        }
-    }
-
-    void unlock() {
-        try {
-            distributedLock.unlock(String.valueOf(transactionInfo.getSellerId()));
-        } finally {
-            distributedLock.unlock(String.valueOf(transactionInfo.getBuyerId()));
-        }
     }
 
     public String getId() {
--- src/test/java/cn/xpbootcamp/legacy_code/WalletTransactionTest.java
+++ src/test/java/cn/xpbootcamp/legacy_code/WalletTransactionTest.java
@@ -2,12 +2,11 @@ package cn.xpbootcamp.legacy_code;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 import cn.xpbootcamp.legacy_code.enums.STATUS;
 import cn.xpbootcamp.legacy_code.service.WalletService;
-import cn.xpbootcamp.legacy_code.utils.lock.DistributedLock;
+import cn.xpbootcamp.legacy_code.utils.lock.DoubleLockManager;
 import cn.xpbootcamp.legacy_code.vo.TransactionInfo;
 import cn.xpbootcamp.legacy_code.vo.exception.InvalidTransactionInfoException;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,99 +19,22 @@ class WalletTransactionTest {
     private long sellerId = 2L;
     private double amount = 10.0;
     private TransactionInfo transactionInfo ;
-    private DistributedLock lock = mock(DistributedLock.class);
+    private DoubleLockManager lockManager = mock(DoubleLockManager.class);
+    private DoubleLockManager.DoubleLock lock = mock(DoubleLockManager.DoubleLock.class);
     private WalletService walletService = mock(WalletService.class);
 
     @BeforeEach
     public void init() throws InvalidTransactionInfoException, InvalidTransactionException {
+        when(lockManager.obtainLock(anyLong(), anyLong())).thenReturn(lock);
         transactionInfo = new TransactionInfo(buyerId, sellerId, amount);
-        walletTransaction = new WalletTransaction(transactionInfo, lock, walletService);
+        walletTransaction = new WalletTransaction(transactionInfo, lockManager, walletService);
     }
 
     @Test
     void should_throw_InvalidTransactionException_when_transaction_info_is_null() {
-        assertThatThrownBy(() -> new WalletTransaction(null, lock, walletService))
+        assertThatThrownBy(() -> new WalletTransaction(null, lockManager, walletService))
                 .isInstanceOf(InvalidTransactionException.class)
                 .hasMessage("This is an invalid transaction");
-    }
-
-    @Test
-    void should_return_false_when_not_locked() throws InvalidTransactionException {
-        // given
-        when(lock.lock(anyString())).thenReturn(false);
-
-        // when
-        boolean result = walletTransaction.execute();
-
-        // then
-        assertThat(result).isFalse();
-    }
-
-    @Test
-    void should_get_double_lock() {
-        // given
-        when(lock.lock(String.valueOf(buyerId))).thenReturn(true);
-        when(lock.lock(String.valueOf(sellerId))).thenReturn(true);
-
-        // when
-        boolean isGetLock = walletTransaction.tryLock();
-
-        // then
-        assertThat(isGetLock).isTrue();
-    }
-
-    @Test
-    void should_return_false_when_get_buyer_lock_fail() {
-        // given
-        when(lock.lock(String.valueOf(buyerId))).thenReturn(false);
-
-        // when
-        boolean isGetLock = walletTransaction.tryLock();
-
-        // then
-        assertThat(isGetLock).isFalse();
-    }
-
-    @Test
-    void should_return_false_when_get_seller_lock_fail() {
-        // given
-        when(lock.lock(String.valueOf(buyerId))).thenReturn(true);
-        when(lock.lock(String.valueOf(sellerId))).thenReturn(false);
-        doNothing().when(lock).unlock(String.valueOf(buyerId));
-
-        // when
-        boolean isGetLock = walletTransaction.tryLock();
-
-        // then
-        assertThat(isGetLock).isFalse();
-        verify(lock, times(1)).unlock(String.valueOf(buyerId));
-    }
-
-    @Test
-    void should_unlock() {
-        // given
-        doNothing().when(lock).unlock(String.valueOf(buyerId));
-        doNothing().when(lock).unlock(String.valueOf(sellerId));
-
-        // when
-        walletTransaction.unlock();
-
-        // then
-        verify(lock, times(1)).unlock(String.valueOf(buyerId));
-        verify(lock, times(1)).unlock(String.valueOf(sellerId));
-    }
-
-    @Test
-    void should_unlock_buyer_when_unlock_seller_fail() {
-        // given
-        doThrow(RuntimeException.class).when(lock).unlock(String.valueOf(sellerId));
-        doNothing().when(lock).unlock(String.valueOf(buyerId));
-
-        // when
-        assertThatThrownBy(() -> walletTransaction.unlock()).isInstanceOf(RuntimeException.class);
-
-        // then
-        verify(lock, times(1)).unlock(String.valueOf(buyerId));
     }
 
     @Test
@@ -130,11 +52,22 @@ class WalletTransactionTest {
     }
 
     @Test
+    void should_return_false_when_not_locked() {
+        // given
+        when(lock.tryLock()).thenReturn(false);
+
+        // when
+        boolean result = walletTransaction.execute();
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
     void should_return_false_when_move_money_failed() {
         // given
+        when(lock.tryLock()).thenReturn(true);
         when(walletService.moveMoney(buyerId, sellerId, amount)).thenReturn(false);
-        when(lock.lock(String.valueOf(buyerId))).thenReturn(true);
-        when(lock.lock(String.valueOf(sellerId))).thenReturn(true);
 
         // when
         boolean result = walletTransaction.doMoveMoney();
@@ -142,5 +75,19 @@ class WalletTransactionTest {
         // then
         assertThat(result).isFalse();
         assertThat(walletTransaction.getStatus()).isEqualTo(STATUS.FAILED);
+    }
+
+    @Test
+    void should_return_true_when_move_money_success() {
+        // given
+        when(lock.tryLock()).thenReturn(true);
+        when(walletService.moveMoney(buyerId, sellerId, amount)).thenReturn(true);
+
+        // when
+        boolean result = walletTransaction.doMoveMoney();
+
+        // then
+        assertThat(result).isTrue();
+        assertThat(walletTransaction.getStatus()).isEqualTo(STATUS.EXECUTED);
     }
 }
